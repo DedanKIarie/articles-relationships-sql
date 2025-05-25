956d96ed191d5356086167e23836a98ac294e74c
DedanKIarie <dedanbroo@gmail.com>
2025-05-25 20:30:02 +0300

    added a database setup script

---
+++ scripts/setup_db.py
@@ -0,0 +1,12 @@
+from lib.db.connection import get_connection
+
+def setup_database():
+    conn = get_connection()
+    with open('lib/db/schema/sql') as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close
+
+if __name__ == '__main__':
+    setup_database()
+    print("Database setup completed")
