96a28274faa27e5b5304b22e28b7b068cee236b6
DedanKIarie <dedanbroo@gmail.com>
2025-05-25 20:36:25 +0300

    implement author class

---
+++ lib/models/author.py
@@ -0,0 +1,29 @@
+from lib.db.connection import get_connection
+
+class Author:
+    def __init__(self, name, id = None):
+        self.name = name
+        self.id = id
+    def save(self):
+        conn  = get_connection()
+        cursor = conn.cursor()
+        if self.id:
+            cursor.execute("UPDATE authors SET name=? Where id=?", (self.name, self.id))
+        else:
+            cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+            self.id = cursor.lastrowid
+
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE id=?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(row['name'], row['id'])
+        
+        return None
